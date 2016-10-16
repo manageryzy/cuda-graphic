f0ad50bc7322a9cadb1310ac58c8ec6f1be52a24
manageryzy <manageryzy@gmail.com>
2016-10-16 23:55:12 +0800

    改成了模板。稍微像一点cpp了

--- gui/GraphicStyle.cpp
+++ gui/GraphicStyle.cpp
@@ -1,101 +1,45 @@
 #include "stdafx.h"
 #include "GraphicStyle.h"
 
-#define LOAD_POINT(X,Y) \
-ar >> len;\
-for (auto i = 0; i < len; i++)\
-{\
-	int t;\
-	Y data;\
-	ar >> t;\
-	ar >> data;\
-	set ## X ## AtFrame(data,t);\
-}
 
-#define STORE_POINT(X) \
-ar << X ## .size();\
-for (auto & p : X)\
-{\
-	ar << p.first;\
-	ar << p.second;\
+GraphicBasicPoint GraphicPoint::atFrame(int frame)
+{
+	GraphicBasicPoint pt;
+	pt.x = x.atFrame(frame);
+	pt.y = y.atFrame(frame);
+	pt.color = color.atFrame(frame);
+	pt.width = width.atFrame(frame);
+	pt.glowColor = glowColor.atFrame(frame);
+	pt.glowWidth = glowWidth.atFrame(frame);
+	pt.shadowColor = shadowColor.atFrame(frame);
+	pt.shadowWidth = shadowWidth.atFrame(frame);
 }
 
 IMPLEMENT_SERIAL(GraphicPoint, CObject, 1)
-
 void GraphicPoint::Serialize(CArchive & ar)
 {
 	int len;
 	if (ar.IsLoading())
 	{
-		LOAD_POINT(x, float);
-		LOAD_POINT(y, float);
-		LOAD_POINT(color, int);
-		LOAD_POINT(width, float);
-		LOAD_POINT(glowColor, int);
-		LOAD_POINT(glowWidth, float);
-		LOAD_POINT(shadowColor, int);
-		LOAD_POINT(shadowWidth, float);
+		x.Serialize(ar);
+		y.Serialize(ar);
+		color.Serialize(ar);
+		width.Serialize(ar);
+		glowColor.Serialize(ar);
+		glowWidth.Serialize(ar);
+		shadowColor.Serialize(ar);
+		shadowWidth.Serialize(ar);
 	}
 	else if (ar.IsStoring())
 	{
-		STORE_POINT(x);
-		STORE_POINT(y);
-		STORE_POINT(color);
-		STORE_POINT(width);
-		STORE_POINT(glowColor);
-		STORE_POINT(glowWidth);
-		STORE_POINT(shadowColor);
-		STORE_POINT(shadowWidth);
+		x.Serialize(ar);
+		y.Serialize(ar);
+		color.Serialize(ar);
+		width.Serialize(ar);
+		glowColor.Serialize(ar);
+		glowWidth.Serialize(ar);
+		shadowColor.Serialize(ar);
+		shadowWidth.Serialize(ar);
 	}
 }
 
-#undef STORE_POINT
-#undef LOAD_POINT
-
-
-//void GraphicPoint::setxAtFrame(float x, int frame)
-//{
-//	if (this->x.size() == 0)
-//	{
-//		this->x.push_back(std::pair<int, float>(frame, x));
-//		return;
-//	}
-//
-//	for (auto i = this->x.begin(); i != this->x.end(); ++i)
-//	{
-//		if (frame < i->first)
-//		{
-//			this->x.insert(i, std::pair<int, float>(frame, x));
-//			break;
-//		}
-//	}
-//}
-#define POINT_SET_FRAME(X,Y) \
-void GraphicPoint::set ## X ## AtFrame( Y  X , int frame)\
-{\
-	if (this-> ## X ## .size() == 0)\
-	{\
-		this-> ## X ## .push_back(std::pair<int, Y >(frame, X ));\
-		return;\
-	}\
-\
-	for (auto i = this-> ## X ## .begin(); i != this-> ## X ## .end(); ++i)\
-	{\
-		if (frame < i->first)\
-		{\
-			this-> ## X ## .insert(i, std::pair<int, Y >(frame, X ));\
-			break;\
-		}\
-	}\
-}
-
-POINT_SET_FRAME(x, float)
-POINT_SET_FRAME(y, float)
-POINT_SET_FRAME(color, int)
-POINT_SET_FRAME(width, float)
-POINT_SET_FRAME(glowColor, int)
-POINT_SET_FRAME(glowWidth, float)
-POINT_SET_FRAME(shadowColor, int)
-POINT_SET_FRAME(shadowWidth, float)
-
-#undef POINT_SET_FRAME
--- gui/GraphicStyle.h
+++ gui/GraphicStyle.h
@@ -15,30 +15,123 @@ public:
 	float shadowWidth;
 } GraphicBasicPoint;
 
+template<typename T>
+class GraphicAttrTimeline : public CObject
+{
+public:
+	std::vector<std::pair<int, T>> attr;
+	T atFrame(int frame);
+	void setAttrAtFrame(T val, int frame);
+	void Serialize(CArchive& ar);
+};
+
+template<typename T>
+inline T GraphicAttrTimeline<T>::atFrame(int frame)
+{
+	if (attr.size() == 0)
+	{
+		return 0;
+	}
+
+	T attr1, attr2;
+	int frame1 = -1, frame2 = -1;
+
+	for (auto it = attr.begin(); it != attr.end(); ++it)
+	{
+		if (frame < it.first)
+		{
+			frame2 = it.first;
+			attr2 = it.second;
+		}
+	}
+	for (auto it = attr.end(); it != attr.begin; --it)
+	{
+		if (frame > it.first)
+		{
+			frame1 = it.first;
+			attr1 = it.second;
+		}
+	}
+
+	if (frame1 == -1 && frame2 == -1)
+	{
+		return 0;
+	}
+
+	if (frame1 == -1)
+		return attr2;
+
+	if (frame2 == -1)
+		return attr1;
+
+	return ((frame2 - frame)*attr2 + (frame - frame1)*attr1) / (frame2 - frame1);
+}
+
+template<typename T>
+inline void GraphicAttrTimeline<T>::setAttrAtFrame(T val, int frame)
+{
+	if (attr.size() == 0)
+	{
+		attr.push_back(std::pair<int, T >(frame, val ));
+		return;
+	}
+
+	for (auto i = attr.begin(); i != attr.end(); ++i)
+	{
+		if (frame < i->first)
+		{
+			attr.insert(i, std::pair<int, T >(frame, val ));
+			break;
+		}
+	}
+}
+
+template<typename T>
+inline void GraphicAttrTimeline<T>::Serialize(CArchive & ar)
+{
+	CObject::Serialize(ar);
+	if (ar.IsLoading())
+	{
+		int len;
+		ar >> len;
+		for (auto i = 0; i < len; i++)
+		{
+			int t;
+			T val;
+			ar >> t;
+			ar >> val;
+			setAttrAtFrame(val, t);
+		}
+	}
+	else if(ar.IsStoring())
+	{
+		ar << attr.size();
+		for (auto & i : attr)
+		{
+			ar << i.first;
+			ar << i.second;
+		}
+	}
+}
+
+
 class GraphicPoint :
 CObject
 {
 public :
 	//frame value pair of attr
-	std::vector<std::pair<int,float>> x;
-	std::vector<std::pair<int,float>> y;
-	std::vector<std::pair<int,DWORD>> color;
-	std::vector<std::pair<int,float>> width;
-	std::vector<std::pair<int,DWORD>> glowColor;
-	std::vector<std::pair<int,float>> glowWidth;
-	std::vector<std::pair<int,DWORD>> shadowColor;
-	std::vector<std::pair<int,float>> shadowWidth;
+	GraphicAttrTimeline<float> x;
+	GraphicAttrTimeline<float> y;
+	GraphicAttrTimeline<DWORD> color;
+	GraphicAttrTimeline<float> width;
+	GraphicAttrTimeline<DWORD> glowColor;
+	GraphicAttrTimeline<float> glowWidth;
+	GraphicAttrTimeline<DWORD> shadowColor;
+	GraphicAttrTimeline<float> shadowWidth;
 
 	GraphicBasicPoint atFrame(int frame);
-	void setxAtFrame(float x, int frame);
-	void setyAtFrame(float y, int frame);
-	void setcolorAtFrame(int color, int frame);
-	void setwidthAtFrame(float width, int frame);
-	void setglowColorAtFrame(int glowColor, int frame);
-	void setglowWidthAtFrame(float glowWidth, int frame);
-	void setshadowColorAtFrame(int shadowColor, int frame);
-	void setshadowWidthAtFrame(float shadowWidth, int frame);
 
 	DECLARE_SERIAL(GraphicPoint)
 	virtual void Serialize(CArchive& ar);
 };
+
