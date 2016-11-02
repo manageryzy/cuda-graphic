b5ab954c13afca089179a62e6e3a3504cf997dc8
manageryzy <manageryzy@gmail.com>
2016-11-02 10:42:46 +0800

    完成了绘制圆

--- gui/GraphicToolAddCircle.cpp
+++ gui/GraphicToolAddCircle.cpp
@@ -2,6 +2,10 @@
 #include "GraphicToolAddCircle.h"
 #include "guiDoc.h"
 #include "guiView.h"
+#include "resource.h"
+
+#define STATE_NONE 0
+#define STATE_PT1 1
 
 GraphicToolAddCircle::GraphicToolAddCircle()
 {
@@ -12,13 +16,75 @@ GraphicToolAddCircle::~GraphicToolAddCircle()
 {
 }
 
-bool GraphicToolAddCircle::onLButtonDown(void *)
+bool GraphicToolAddCircle::onLButtonDown(void * point)
 {
+	GRA_INIT_DOC;
+	GRA_INIT_CAMERA;
+
+	if (state == STATE_NONE)
+	{
+		view->createing = new Graphic();
+		view->createing->init(GRA_CIRCLE, L"unnamed circle");
+		view->createing->graphicCircle->init();
+		view->createing->graphicCircle->x.setAttrAtFrame(worldPoint.x, view->frame);
+		view->createing->graphicCircle->y.setAttrAtFrame(worldPoint.y, view->frame);
+		view->selectedGraphic.clear();
+		state = STATE_PT1;
+	}
+	else if (state == STATE_PT1)
+	{
+		auto centerX = view->createing->graphicCircle->x.atFrame(view->frame);
+		auto centerY = view->createing->graphicCircle->y.atFrame(view->frame);
+		auto r = sqrtf((centerX - worldPoint.x)*(centerX - worldPoint.x) + (centerY - worldPoint.y)*(centerY - worldPoint.y));
+		view->createing->graphicCircle->r.setAttrAtFrame(r, view->frame);
+		view->endCreating();
+		state = STATE_NONE;
+
+		//TODO: add history here
+	}
+	else
+	{
+		throw "unknown state";
+	}
+
 	return false;
 }
 
-bool GraphicToolAddCircle::onMouseMove(void *)
+bool GraphicToolAddCircle::onMouseMove(void * point)
 {
+	GRA_INIT_DOC;
+	GRA_INIT_CAMERA;
+
+	if (state == STATE_PT1)
+	{
+		auto centerX = view->createing->graphicCircle->x.atFrame(view->frame);
+		auto centerY = view->createing->graphicCircle->y.atFrame(view->frame);
+		auto r = sqrtf((centerX - worldPoint.x)*(centerX - worldPoint.x) + (centerY - worldPoint.y)*(centerY - worldPoint.y));
+		view->createing->graphicCircle->r.setAttrAtFrame(r, view->frame);
+	}
+
+	return false;
+}
+
+bool GraphicToolAddCircle::onRButtonUp(void *)
+{
+	if (state == STATE_PT1)
+	{
+		cancel();
+		return true;
+	}
+
+	return false;
+}
+
+bool GraphicToolAddCircle::onESC(void *)
+{
+	if (state == STATE_PT1)
+	{
+		cancel();
+		return true;
+	}
+
 	return false;
 }
 
@@ -32,6 +98,19 @@ void GraphicToolAddCircle::redoHistory(GraphicHistory)
 
 void GraphicToolAddCircle::reset()
 {
+	state = STATE_NONE;
+}
+
+void GraphicToolAddCircle::cancel()
+{
+	auto res = MessageBox(view->GetSafeHwnd(), L"cancel?", L"", MB_YESNO);
+	if (res == IDYES)
+	{
+		delete view->createing;
+		view->createing = nullptr;
+		view->editTool = nullptr;
+		state = STATE_NONE;
+	}
 }
 
 
